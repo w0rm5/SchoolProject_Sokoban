f732d3ce67149c2411c47e1bf9dffbcfa24c69a3
w0rm5 <47029568+w0rm5@users.noreply.github.com>
2021-08-04 01:02:35 +0700

    done level select and updated transparent sprites

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,16 +8,58 @@ public class MainMenu : MonoBehaviour
 {
     private AudioManager audioManager;
 
+    public TextMeshProUGUI TitleText;
+    public GameObject MainMenuObject;
+    public GameObject LevelSelectPanel;
+    public GameObject BackButton;
+
     private void Start()
     {
         audioManager = FindObjectOfType<AudioManager>();
         audioManager.Play("mainBgMusic");
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape) && LevelSelectPanel.activeSelf)
+        {
+            BackMainMenu();
+        }
+    }
+
     public void StartGame()
     {
         audioManager.Play("btnClickSound");
         SceneManager.LoadScene("Level1");
+    }
+
+    public void ShowLevelSelectMenu()
+    {
+        audioManager.Play("btnClickSound");
+        TitleText.text = "SELECT LEVEL";
+        MainMenuObject.SetActive(false);
+        LevelSelectPanel.SetActive(true);
+        BackButton.SetActive(true);
+    }
+
+    public void SelectLevel(string level)
+    {
+        audioManager.Play("btnClickSound");
+        SceneManager.LoadScene("Level" + level);
+    }
+
+    public void BackMainMenuClick()
+    {
+        audioManager.Play("btnClickSound");
+        BackMainMenu();
+    }
+
+    private void BackMainMenu()
+    {
+        TitleText.text = "SOKOBAN GAME";
+        LevelSelectPanel.SetActive(false);
+        BackButton.SetActive(false);
+        MainMenuObject.SetActive(true);
     }
 
     public void QuitGame()
